fec4fd616dc868baa0b41e2fe8a52d09f5cf3c02
bbbyeol <forbyeol@naver.com>
2024-03-13 22:51:32 +0900

    springdoc 의존성 추가
    swagger

---
+++ src/main/java/org/zerock/b02/config/CustomServletConfig.java
@@ -0,0 +1,17 @@
+package org.zerock.b02.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+@EnableWebMvc
+public class CustomServletConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+    }
+
+}
