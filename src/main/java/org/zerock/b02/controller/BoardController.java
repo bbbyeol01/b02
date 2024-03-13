76832a0ed2efe9e883bec370cdfeeb0e1b7a1ade
bbbyeol <forbyeol@naver.com>
2024-03-13 14:16:40 +0900

    게시판 글 조회, 등록 구현
    register(GET), register(POST)
    read, register

--- src/main/java/org/zerock/b02/controller/BoardController.java
+++ src/main/java/org/zerock/b02/controller/BoardController.java
@@ -1,11 +1,17 @@
 package org.zerock.b02.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.Value;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.b02.dto.BoardDTO;
 import org.zerock.b02.dto.PageRequestDTO;
 import org.zerock.b02.dto.PageResponseDTO;
 import org.zerock.b02.service.BoardService;
@@ -27,4 +33,44 @@ public class BoardController {
         model.addAttribute("pageResponseDTO", pageResponseDTO);
 
     }
+
+    @GetMapping("/register")
+    public void registerGet() {
+
+    }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid BoardDTO boardDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        log.info("board register(POST).............");
+
+        if(bindingResult.hasErrors()){
+            log.info("error!");
+
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/board/register";
+        }
+
+        log.info("boardDTO " + boardDTO);
+
+        Long bno = boardService.register(boardDTO);
+        redirectAttributes.addFlashAttribute("result", bno);
+
+        return "redirect:/board/list";
+
+
+    }
+
+    @GetMapping("/read")
+    public void read(Long bno, PageRequestDTO pageRequestDTO, Model model){
+
+        BoardDTO boardDTO = boardService.readOne(bno);
+
+        log.info("[read] " + boardDTO);
+
+        model.addAttribute("boardDTO", boardDTO);
+    }
+
+
 }
