99089283b6a7eb5307b7c27aa5a54149065bd8e5
bbbyeol <forbyeol@naver.com>
2024-03-14 15:50:47 +0900

    ㅇㅇ

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,4 +1,4 @@
-async function getList(bno, page, size, goLast){
+async function getList({bno, page, size, goLast}){
     const result = await axios.get(`/replies/list/${bno}`, {params: {page, size}});
 
     return result.data;
